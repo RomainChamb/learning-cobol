0e04a24ecb696e3317a2fe68b3984f207ff5c85c
Romain <romain.code@pm.me>
2025-03-11 19:12:12 +0100

    Add comment

--- hello.cob
+++ hello.cob
@@ -1,5 +1,5 @@
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. HELLO-WORLD.
-        PROCEDURE DIVISION.
-          DISPLAY "Hello, world !".
-          STOP RUN.
+        IDENTIFICATION DIVISION.           *> Start of script
+        PROGRAM-ID. HELLO-WORLD.           *> Script ID
+        PROCEDURE DIVISION.                *> Main section
+          DISPLAY "Hello, world !".        *> Text printing
+          STOP RUN.                        *> End of script
